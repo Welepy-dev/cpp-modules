265ee7e2a8ca9faf990c2cfe9c19be1c7dd57814
Welepy-dev <mwsilva.contato@gmail.com>
2025-04-13 18:22:05 +0100

    code refactor

--- CPP-Module-01/ex00/inc/Zombie.hpp
+++ CPP-Module-01/ex00/inc/Zombie.hpp
@@ -3,26 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 18:25:38 by marcilsv          #+#    #+#             */
-/*   Updated: 2025/04/06 15:44:19 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/04/13 17:46:28 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
+
 # include <string>
 
-class Zombie {
-public:
-	void	announce(void);
-	Zombie	*newZombie(std::string name);
-	void	randomChump(std::string name);
-	Zombie() ;
-	~Zombie() ;
-private:
-	std::string	name;
+class	Zombie
+{
+	public:
+		void	announce(void);
+		void	randomChump(std::string name);
+
+		Zombie	*newZombie(std::string name);
+
+		Zombie() ;
+		~Zombie() ;
+	private:
+		std::string	name;
 };
 
 #endif
