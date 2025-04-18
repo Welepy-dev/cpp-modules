daf9cfecbcd6619c4060232e11c68ca96320fa8e
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2025-04-18 10:44:55 +0000

    implementing the HumanA class and building the constructor

--- CPP-Module-01/ex03/inc/HumanA.hpp
+++ CPP-Module-01/ex03/inc/HumanA.hpp
@@ -3,28 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   HumanA.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 14:50:47 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/04/07 15:41:53 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/04/18 10:40:55 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANA_HPP
 # define HUMANA_HPP
+
 # include <string>
 # include "Weapon.hpp"
 
 class HumanA
 {
-  public:
-    HumanA(std::string name, Weapon &weapon):
-            name(name), weapon(weapon) {}
-    ~HumanA();
+	private:
+		std::string name;
+		Weapon  &weapon;
 
-    std::string name;
-    Weapon  &weapon;
-    void  attack(void);
+	public:
+		HumanA(std::string Name, Weapon &Weapon): name(name), weapon(weapon) {}
+		~HumanA();
+
+		void  attack(void);
 };
 
 #endif // !HUMANA_HPP
