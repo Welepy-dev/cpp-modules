370ee63dcaa0030062a711296a0616b051734698
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2025-04-18 10:46:44 +0000

    Implementing the HumanB class and its setters

--- CPP-Module-01/ex03/inc/HumanB.hpp
+++ CPP-Module-01/ex03/inc/HumanB.hpp
@@ -3,31 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   HumanB.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 15:45:03 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/04/07 22:51:08 by welepy           ###   ########.fr       */
+/*   Updated: 2025/04/18 10:37:44 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANB_HPP
 #define HUMANB_HPP
 # include <string>
+
 # include "Weapon.hpp"
 
 class HumanB
 {
-  public:
-    HumanB();
-    ~HumanB();
-    HumanB(std::string name);
+	public:
+		HumanB();
+		~HumanB();
+		HumanB(std::string Name);
 
-    void  setWeapon(Weapon &weapon);
-    std::string name;
-    Weapon  *weapon;
-    void  attack(void);
-  private:
-  
+		void  setWeapon(Weapon &Weapon);
+		void  attack(void);
+	
+	private:
+		std::string name;
+		Weapon  *weapon;
+	
 };
 
 #endif
