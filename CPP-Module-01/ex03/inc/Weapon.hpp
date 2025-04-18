53cb16ecce771af893cb2c5c426cada14339543d
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2025-04-18 10:49:39 +0000

    weapon hpp

--- CPP-Module-01/ex03/inc/Weapon.hpp
+++ CPP-Module-01/ex03/inc/Weapon.hpp
@@ -3,29 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 14:54:18 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/04/07 14:56:31 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/04/18 10:19:20 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEAPON_HPP
 # define WEAPON_HPP
+
 # include <string>
 
 class Weapon
 {
-  private:
-    std::string type;
+	private:
+		std::string type;
 
-  public:
-    const std::string &getType(void);
-    void  setType(std::string newType);
+	public:
+		const std::string &getType(void);
+		void  setType(std::string newType);
 
-    Weapon();
-    ~Weapon();
-    Weapon(std::string name);
+		Weapon();
+		~Weapon();
+		Weapon(std::string name);
 };
 
 #endif
