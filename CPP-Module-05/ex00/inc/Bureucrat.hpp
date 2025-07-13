54bf82c174fd5bc4a96a5c43fd56bb6adf2f87e5
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2025-07-13 22:24:37 +0000

    changing code shape

--- CPP-Module-05/ex00/inc/Bureucrat.hpp
+++ CPP-Module-05/ex00/inc/Bureucrat.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureucrat.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 16:10:19 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/06/02 16:52:46 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/07/13 21:59:21 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@
 # include <iostream>
 class Bureucrat 
 {
-  public:
-    Bureucrat(void);
-    Bureucrat(std::string name, int grade);
-    Bureucrat(const Bureucrat &other);
-    Bureucrat &operator=(const Bureucrat &other);
-    ~Bureucrat(void);
+	public:
+		Bureucrat(void);
+		Bureucrat(std::string name, int grade);
+		Bureucrat(const Bureucrat &other);
+		Bureucrat &operator=(const Bureucrat &other);
+		~Bureucrat(void);
 
-    std::string getName(void) const;
+		std::string getName(void) const;
 
-    int         getGrade(void) const;
-    void        setGrade(const int grade);
-    
-    void        incrementGrade(void);
-    void        decrementGrade(void);
-  
-  private:
-    std::string name;
-    int               grade;
+		int		getGrade(void) const;
+		void	setGrade(const int grade);
+
+		void	incrementGrade(void);
+		void	decrementGrade(void);
+
+	private:
+		const std::string	name;
+		int					grade;
 };
 
 std::ostream& operator<<(std::ostream &out, const Bureucrat &bureucrat);
--- CPP-Module-05/ex00/src/Bureucrat.cpp
+++ CPP-Module-05/ex00/src/Bureucrat.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureucrat.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 16:45:08 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/06/23 13:07:14 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/07/13 22:06:00 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,11 @@
 #include "../inc/GradeTooHighException.hpp"
 #include "../inc/GradeTooLowException.hpp"
 
-std::string Bureucrat::getName(void) const
-{
-  return (name);
-}
+std::string Bureucrat::getName(void) const { return (name); }
 
-void   Bureucrat::setGrade(const int grade)
-{
-  this->grade = grade;
-}
+void   Bureucrat::setGrade(const int grade) { this->grade = grade; }
 
-int   Bureucrat::getGrade(void) const
-{
-  return (grade);
-}
+int   Bureucrat::getGrade(void) const { return (grade); }
 
 void  Bureucrat::incrementGrade(void)
 {
@@ -42,35 +33,31 @@ void  Bureucrat::decrementGrade(void)
 	if (grade == 150) {
 		throw GradeTooLowException("Grade at its lowest!");
 	}
-  grade++;
+	grade++;
 }
 
 std::ostream& operator<<(std::ostream &out, const Bureucrat &bureucrat)
 {
-  out << bureucrat.getName() << ", bureucrat grade " << bureucrat.getGrade();
-  return (out);
+	out << bureucrat.getName() << ", bureucrat grade " << bureucrat.getGrade();
+	return (out);
+}
+
+Bureucrat &Bureucrat::operator=(const Bureucrat &other)
+{
+	std::cout << "Bureucrat's Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->setGrade(other.getGrade());
+	return (*this);
+}
+
+Bureucrat::Bureucrat(const Bureucrat &other)
+{
+	std::cout << "Bureucrat's copy constructor operator called" << std::endl;
+
+	*this = other;
 }
 
 Bureucrat::Bureucrat(void) { std::cout << "Bureucrat's default constructor called" << std::endl; }
 Bureucrat::~Bureucrat(void) { std::cout << "Bureucrat's default destructor called" << std::endl; }
 Bureucrat::Bureucrat(std::string name, int grade): name(name), grade(grade)
 { std::cout << "Bureucrat's parametized constructor called" << std::endl; }
-
-Bureucrat &Bureucrat::operator=(const Bureucrat &other)
-{
-  std::cout << "Bureucrat's Copy assignment operator called" << std::endl;
-  if (this != &other)
-  {
-    this->name = other.name;
-    this->grade = other.grade;
-  }
-  return (*this);
-}
-
-Bureucrat::Bureucrat(const Bureucrat &other)
-{
-  std::cout << "Bureucrat's copy constructor operator called" << std::endl;
-
-  *this = other;
-}
-
