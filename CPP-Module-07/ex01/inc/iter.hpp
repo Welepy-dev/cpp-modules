fa9ae87c89d7be150bc1b738dbf4636e661a2dbf
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2025-10-01 15:03:27 +0000

    ex01 cpp07 not finished

--- CPP-Module-07/ex01/inc/iter.hpp
+++ CPP-Module-07/ex01/inc/iter.hpp
@@ -5,8 +5,11 @@ T add(T a, T b) {
 }
 */
 
+//int (*fp)(int, int);   // fp is a pointer to a function taking (int,int) and returning int
+
+
 template <typename T>
-void	iter(T *a, const T size, void f())
+void	iter(T *a, const T size, void(*f)(T))
 {
 	
 }
