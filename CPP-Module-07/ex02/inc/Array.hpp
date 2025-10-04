c39c4db2ef94f018789427b990f014d50f9bcdc8
Welepy-dev <mwsilva.contato@gmail.com>
2025-10-04 19:05:48 +0000

    module 07 finished

--- CPP-Module-07/ex02/inc/Array.hpp
+++ CPP-Module-07/ex02/inc/Array.hpp
@@ -1,21 +1,70 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-template <class T>
+# include <cstddef>
+# include <cstring>
+# include <iostream>
+
+template <typename T>
 class	Array
 {
 	public:
-		Array(void);
-		Array(unsigned n); // Tip: Try to compile int * a = new int(); then display *a.
+		Array(void): _array(NULL), _size(0) { };
+		Array(unsigned int n): _array(new T[n]()), _size(n) { }; // the _array(new T[n]() works the same way as std::memset(_array, 0, _size); )
 		Array(const Array &other);
 		Array &operator=(const Array &other);
-		~Array(void);
+		~Array(void) { delete[] _array; };
 
-		unsigned size(void) const;
+		unsigned size(void) const { return (_size); };
+
+    T &operator[](unsigned int index);
+    const T &operator[](unsigned int index) const;
 
 	private:
-		T			_array;
-		unsigned	_size;
+		T			*_array;
+		unsigned int	_size;
+};
+
+template <typename T>
+T& Array<T>::operator[](unsigned int index) {
+    if (index >= _size)
+        throw std::out_of_range("Index out of bounds");
+    return _array[index];
 }
+
+// Const version (for const arrays)
+template <typename T>
+const T& Array<T>::operator[](unsigned int index) const {
+    if (index >= _size)
+        throw std::out_of_range("Index out of bounds");
+    return _array[index];
+}
+
+template<typename T>
+Array<T>::Array(const Array<T> &other): _array(new T[other._size]) , _size(other._size)
+{
+	std::cout << "Array Copy constructor called" << std::endl;
+
+	for (unsigned int i = 0; i < _size; ++i)
+		_array[i] = other._array[i];
+	*this = other;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &other)
+{
+	std::cout << "Array Copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		delete[] _array;
+		_array = new T[_size];
+		for (unsigned int i = 0; i < _size; ++i)
+			_array[i] = other._array[i];
+	}
+	return (*this);
+}
+
+// • When accessing an element with the [ ] operator, if its index is out of bounds, an
+// std::exception is thrown.
 
 #endif
--- CPP-Module-07/ex02/src/main.cpp
+++ CPP-Module-07/ex02/src/main.cpp
@@ -1,1 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcsilv <marcsilv@student.42.f4>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 18:27:33 by marcsilv          #+#    #+#             */
+/*   Updated: 2025/10/04 18:40:06 by marcsilv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/Array.hpp"
+
+int main() {
+    std::cout << "=== Basic construction tests ===" << std::endl;
+
+    // Empty array
+    Array<int> a;
+    std::cout << "Empty array size: " << a.size() << std::endl;
+
+    // Array of 5 ints
+    Array<int> b(5);
+    std::cout << "Array of 5 ints created. Size: " << b.size() << std::endl;
+
+    // Show default initialization
+    std::cout << "Contents of b: ";
+    for (unsigned int i = 0; i < b.size(); i++)
+        std::cout << b[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "\n=== Value assignment tests ===" << std::endl;
+    for (unsigned int i = 0; i < b.size(); i++)
+        b[i] = i * 10;
+    std::cout << "Modified b: ";
+    for (unsigned int i = 0; i < b.size(); i++)
+        std::cout << b[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "\n=== Copy constructor test ===" << std::endl;
+    Array<int> c(b); // deep copy
+    std::cout << "Copy of b created. Contents: ";
+    for (unsigned int i = 0; i < c.size(); i++)
+        std::cout << c[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "Modifying c[0] = 999..." << std::endl;
+    c[0] = 999;
+    std::cout << "c[0]: " << c[0] << ", b[0]: " << b[0] << std::endl;
+
+    std::cout << "\n=== Assignment operator test ===" << std::endl;
+    Array<int> d;
+    d = b;
+    std::cout << "Assigned d = b. Contents: ";
+    for (unsigned int i = 0; i < d.size(); i++)
+        std::cout << d[i] << " ";
+    std::cout << std::endl;
+/*
+    std::cout << "Modifying d[1] = 777..." << std::endl;
+    d[1] = 777;
+    std::cout << "d[1]: " << d[1] << ", b[1]: " << b[1] << std::endl;
+
+    std::cout << "\n=== Out of bounds test ===" << std::endl;
+    try {
+        std::cout << "Accessing b[10]..." << std::endl;
+        std::cout << b[10] << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }*/
+
+    std::cout << "\n=== Const correctness test ===" << std::endl;
+    const Array<int> e(b);
+    std::cout << "Const copy contents: ";
+    for (unsigned int i = 0; i < e.size(); i++)
+        std::cout << e[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "\n=== Template type test (strings) ===" << std::endl;
+    Array<std::string> strs(3);
+    strs[0] = "Hello";
+    strs[1] = "World";
+    strs[2] = "!";
+    for (unsigned int i = 0; i < strs.size(); i++)
+        std::cout << strs[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "\nAll tests completed successfully!" << std::endl;
+    return 0;
+}
