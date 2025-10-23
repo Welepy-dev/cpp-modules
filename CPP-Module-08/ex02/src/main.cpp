a2522f4ea2cc380abd016dc643f7b80de68298a2
welepy-dev <mwsilva.contato@gmail.com>
2025-10-23 08:25:31 +0100

    fixed some problems on ex02cpp08

--- CPP-Module-08/ex02/src/main.cpp
+++ CPP-Module-08/ex02/src/main.cpp
@@ -10,20 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Span.hpp"
+#include "../inc/MutantStack.hpp"
 
-int	main()
-{
-	Span sp = Span(5);
-
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
-
-	return (0);
+int	main(void) {
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...] mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
