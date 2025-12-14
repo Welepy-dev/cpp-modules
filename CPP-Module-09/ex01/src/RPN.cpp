a49ab5578200a7a975d2a1cd52753f46129e2002
Welepy-dev <mwsilva.contato@outlook.com>
2025-12-14 13:51:20 +0100

    finished 0901

--- CPP-Module-09/ex01/src/RPN.cpp
+++ CPP-Module-09/ex01/src/RPN.cpp
@@ -35,30 +35,47 @@ short RPN::get_top() {
 }
 
 void RPN::calculate(void) {
-	if (this->_expr.find_first_not_of("0123456789/*-+") != std::string::npos) {
+	if (this->_expr.find_first_not_of(" 0123456789/*-+") != std::string::npos) {
 		std::cerr << "Error." << std::endl;
 		return ;
 	}
 	for (short i = 0; i < static_cast<short>(this->_expr.length()); i++) {
+		if (std::isspace(this->_expr[i]))
+			i++;
 		if ((this->_expr[i] >= '0') && (this->_expr[i] <= '9')) {
 			this->_stack.push(this->char_to_int(this->_expr[i]));
 		} else {
-			if (this->_stack.size() != 2) {
+			if (this->_stack.size() < 2) {
 				std::cerr << "Error." << std::endl;
 				return ;
 			}
 			switch (this->_expr[i])
 			{
+				short a;
+				short b;
 				case '+':
-					this->_result = this->get_top();
+					a = this->get_top();
+					b = this->get_top();
+					this->_stack.push(a + b);
+					break ;
 				case '-':
-				/* code */
-					break;
+					a = this->get_top();
+					b = this->get_top();
+					this->_stack.push(b - a);
+					break ;
 				case '*':
-					/* code */
+					a = this->get_top();
+					b = this->get_top();
+					this->_stack.push(a * b);
 					break;
 				case '/':
-				/* code */
+					a = this->get_top();
+					if (a == 0) {
+						std::cerr << "Error." << std::endl;
+						return ;
+					}
+					b = this->get_top();
+					this->_stack.push(b / a);
 					break;
 				default:
 					std::cerr << "Error." << std::endl;
@@ -66,4 +83,10 @@ void RPN::calculate(void) {
 			}
 		}
 	}
+	if (this->_stack.size() > 1) {
+		std::cerr << "Error." << std::endl;
+		return ;
+	}
+	this->_result = this->_stack.top();
+	std::cout << "result: " << this->_result << std::endl;
 }
